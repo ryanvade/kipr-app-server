cbabb2d3a7e1ef838059f3e077a6bba40073e3b7
Maria Scharth <mschart@siue.edu>
2017-10-31 15:15:02 -0500

    created judgeGUI view and began raw layout of interface

---
+++ resources/views/judgeGUI.blade.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title> Judging Interface </title>
+<style type='text/css'>
+#content {
+	width: 600px;
+	float: left;
+	padding: 5px 20px;
+    font-size: 25px;
+}
+#middleColumn {
+    width: 200px;
+    float: left;
+    padding: 5px 15 px;
+    font-size: 25px;
+}
+</style>
+</head>
+<body>
+<h1 align = "center"> Judge Scoring Interface </h1>
+<section>
+  <h1 align = "center">Team A Scoring</h1>
+</section>
+<section id="content">
+		<h2>Scoring List</h2>
+		<p>Red balls in basket<br><br>Blue balls in basket</p>
+</section>
+<section id="middleColumn">
+    <h2>actual scoring buttons</h2>
+    <p>textbox with plus and minus buttons</p>
+</section>
+</body>
+</html>
+
+
+
+
