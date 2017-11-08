fa8397c7e3fbaf7dc149378a29633e5e42591b12
Ryan Owens <ryanvader82@gmail.com>
2017-11-07 22:06:27 -0600

    Implemented judging middleware

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class MatchController extends Controller
 {
+
+    public function __construct()
+    {
+      $this->middleware('auth:api');
+    }
     /**
       * Update the specified resource in storage.
       *
--- tests/Feature/SubmitScoreTest.php
+++ tests/Feature/SubmitScoreTest.php
@@ -2,15 +2,44 @@
 
 namespace Tests\Feature;
 
+use KIPR\User;
 use KIPR\Match;
 use Tests\TestCase;
 use KIPR\Competition;
+use Laravel\Passport\Passport;
+use Laravel\Passport\ClientRepository;
+use Laravel\Passport\PersonalAccessClient;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SubmitScoreTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_a_judge_can_submit_match_scores()
+    
+    private $token = '';
+    public function setUp()
+    {
+        parent::setUp();
+      
+        $admin = Factory(User::class)->create();
+        $notadmin = Factory(User::class)->create();
+      
+        $clients = new ClientRepository();
+        $client = $clients->createPersonalAccessClient(
+          null, 'judging', env('APP_URL', 'http://localhost')
+      );
+        $accessClient = new PersonalAccessClient();
+        $accessClient->client_id = $client->id;
+        $accessClient->save();
+      
+        Passport::tokensCan([
+            'judge-tournaments' => 'Judge Tournaments',
+            'check-in-teams' => 'Check in Teams',
+        ]);
+      
+        $this->token = $admin->createToken('judging', ['judge-tournaments'])->accessToken;
+    }
+    
+    public function test_only_authenticated_judges_can_submit_match_scores()
     {
         # Given a competition
         $competition = Factory(Competition::class)->create();
@@ -48,9 +77,13 @@ class SubmitScoreTest extends TestCase
         # and a set of match results
         $results = '';
         # post the results to the server
-        $response = $this->json('patch', '/api/match/' . $match->id . '/score', [
-        'results' => json_encode($results)
-      ]);
+        $response = $this->withHeaders([
+          'Accept' => 'application/json',
+          'Authorization' => 'Bearer '. $this->token,
+        ])
+        ->json('patch', '/api/match/' . $match->id . '/score', [
+          'results' => json_encode($results)
+        ]);
 
         $response
                 ->assertStatus(200)
