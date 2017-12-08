64407549854792909f2370b098ede41d3b42c91f
Ryan Owens <ryanvader82@gmail.com>
2017-12-08 17:31:20 -0600

    Team Signin backend complete

--- app/Http/Controllers/CompetitionController.php
+++ app/Http/Controllers/CompetitionController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class CompetitionController extends Controller
 {
-  //
+    /**
+     * Teams - Get the teams registered to a Competition
+     * @param  Competition $competition Competition to get teams for
+     * @return JSON                     All teams of the competition
+     */
+    public function teams(Competition $competition)
+    {
+        return $competition->teams()->get();
+    }
 }
--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -10,6 +10,13 @@ use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
+    /**
+     * Sign In - Sign in a team to a competition
+     * @param  Competition $competition Competition to sign the team into
+     * @param  Team        $team        Team to be signed in
+     * @param  Request     $request     HTTP Request
+     * @return JSON                     JSON Response
+     */
     public function signIn(Competition $competition, Team $team, Request $request)
     {
         $team = $competition->teams()->where('team_id', $team->id)->firstOrFail();
