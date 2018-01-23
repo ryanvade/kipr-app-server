5b97a2d6e8b321c9866244172fbc283a8571b9f6
Ryan Owens <ryanvader82@gmail.com>
2018-01-22 21:46:59 -0600

    Fixed bad merge resolution

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -30,78 +30,87 @@ class TeamController extends Controller
         'team_id' => $team->id,
         'competition_id' => $competition->id,
         'sign_in_time' => $team->pivot->sign_in_at
-    public function getTeamCount() {
-      $count = Team::count();
-      return response()->json([
+      ]);
+    }
+    
+    public function getTeamCount()
+    {
+        $count = Team::count();
+        return response()->json([
         'status' => 'success',
         'team_count' => $count
       ]);
     }
 
-    public function create(CreateTeam $request) {
-      $team = Team::create([
+    public function create(CreateTeam $request)
+    {
+        $team = Team::create([
         'name' => $request->name,
         'email' => $request->email,
         'code' => $request->code
       ]);
-      return response()->json([
+        return response()->json([
         'status' => 'success',
         'team' => $team
       ]);
     }
 
-    public function getAll() {
-      return Team::paginate(20);
+    public function getAll()
+    {
+        return Team::paginate(20);
     }
 
-    public function get(Team $team) {
-      return $team;
+    public function get(Team $team)
+    {
+        return $team;
     }
 
-    public function delete(Team $team) {
-      $team->delete();
-      return response()->json([
+    public function delete(Team $team)
+    {
+        $team->delete();
+        return response()->json([
         'status' => 'success',
         'message' => $team->id . ' deleted'
       ]);
     }
 
-    public function patch(Team $team, UpdateTeam $request) {
-      $team->update([
+    public function patch(Team $team, UpdateTeam $request)
+    {
+        $team->update([
         'name' => $request->name,
         'email' => $request->email,
         'code' => $request->code
       ]);
-      return response()->json([
+        return response()->json([
         'status' => 'success',
         'team' => $team
       ]);
     }
 
-    public function massUpload(Request $request) {
-      $request->validate([
+    public function massUpload(Request $request)
+    {
+        $request->validate([
         'file' => 'required|file'
       ]);
-      $file = $request->file;
-      $count = 0;
-      $invalid = 0;
-      foreach(file($file) as $line) {
-        $values = explode(",", $line);
-        if(count($values) == 3) {
-          if(Team::where('code', $values[0])->count() > 0)
-          {
-            $invalid++;
-          }else {
-            Team::create([
+        $file = $request->file;
+        $count = 0;
+        $invalid = 0;
+        foreach (file($file) as $line) {
+            $values = explode(",", $line);
+            if (count($values) == 3) {
+                if (Team::where('code', $values[0])->count() > 0) {
+                    $invalid++;
+                } else {
+                    Team::create([
               'code' => $values[0],
               'name' => $values[1],
               'email' => $values[2]
             ]);
-            $count++;
-          }
+                    $count++;
+                }
+            }
         }
-      }
-      return response()->json([
+        return response()->json([
         'status' => 'success',
         'message' => $count . ' teams added',
         'ignored' => $invalid . ' teams ignored'
