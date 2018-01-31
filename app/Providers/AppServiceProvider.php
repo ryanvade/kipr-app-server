e568b865171f59c2d6c71fa151dd51cc13d4a6e0
Ryan Owens <ryanvader82@gmail.com>
2018-01-30 22:44:14 -0600

    Removed unnecessary Dusk configs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace KIPR\Providers;
 
-use Laravel\Dusk\DuskServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
+      //
     }
 }
