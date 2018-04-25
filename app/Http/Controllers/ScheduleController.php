ccfb944e901788c78f8f1c9d55837e7b66f9b984
Ryan Owens <ryanvader82@gmail.com>
2018-04-25 10:02:49 -0500

    Added BSD3 License

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -1,3 +1,33 @@
+<!-- Copyright (c) 2018 KISS Institute for Practical Robotics
+
+BSD v3 License
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of KIPR Scoring App nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. -->
 <?php
 
 namespace KIPR\Http\Controllers;
@@ -90,12 +120,12 @@ class ScheduleController extends Controller
                     if ($match->team_A < 0) {
                     } else {
                     }
-                    $match->matchA()->associate($match->matchAObj);  
+                    $match->matchA()->associate($match->matchAObj);
                     unset($match->matchAObj);
                 }
                 if($match->matchBObj) {
                     info("Object B");
-                    $match->matchB()->associate($match->matchBObj);  
+                    $match->matchB()->associate($match->matchBObj);
                     $competition->matches()->save($match->matchBObj);
                     unset($match->matchBObj);
                 }
@@ -126,7 +156,7 @@ class ScheduleController extends Controller
                         $match->results = "{\"score\": 0, \"winner\": $match->team_B, \"loser\": 0}";
                         $competition->matches()->save($match);
                         event(new MatchScored($match));
-                    } 
+                    }
                 }
                 if($match->match_B < 1) {
                     if ($match->team_B < 1) {
@@ -136,7 +166,7 @@ class ScheduleController extends Controller
                         info("BYE FOR A");
                         info($match);
                         event(new MatchScored($match));
-                    } 
+                    }
                 }
             }
 
